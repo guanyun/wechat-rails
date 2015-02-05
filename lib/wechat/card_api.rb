3143fe1fdf8a863725fdde77fec9a290b0afc6a5
Feiping Huang <feiping.huang@yottaa.com>
2015-02-05 23:35:17 +0800

    init string for hash

--- lib/wechat/card_api.rb
+++ lib/wechat/card_api.rb
@@ -106,7 +106,7 @@ class Wechat::CardApi < Wechat::Api
   #       var cardList= res.cardList; // 用户选中的卡券列表信息
   #   }
   # });
-  def js_choose_card(params = {})
+  def js_choose_card(params = Hash.new(''))
     sign_params = {
       app_id: appid,
       times_tamp: Wechat::Utils.get_timestamp,
