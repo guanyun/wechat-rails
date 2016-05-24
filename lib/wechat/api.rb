23480057cdd81bc5123f1714a497db12448f3a35
feipinghuang <feiping.huang@thexstore.cn>
2016-05-24 17:02:42 +0800

    add more api for menu & tag controle

--- lib/wechat/api.rb
+++ lib/wechat/api.rb
@@ -4,8 +4,8 @@ require 'wechat/access_token'
 class Wechat::Api
   attr_reader :access_token, :client, :appid, :secret
 
-  API_BASE = "https://api.weixin.qq.com/cgi-bin/"
-  FILE_BASE = "http://file.api.weixin.qq.com/cgi-bin/"
+  API_BASE = 'https://api.weixin.qq.com/cgi-bin/'
+  FILE_BASE = 'http://file.api.weixin.qq.com/cgi-bin/'
 
   def initialize appid, secret
     @appid = appid
@@ -15,40 +15,52 @@ class Wechat::Api
   end
 
   def users
-    get("user/get")
+    get('user/get')
   end
 
   def user openid
-    get("user/info", params:{openid: openid})
+    get('user/info', params:{openid: openid})
   end
 
   def menu
-    get("menu/get")
+    get('menu/get')
   end
 
   def menu_delete
-    get("menu/delete")
+    get('menu/delete')
   end
 
   def menu_create menu
     # 微信不接受7bit escaped json(eg \uxxxx), 中文必须UTF-8编码, 这可能是个安全漏洞
-    post("menu/create", JSON.generate(menu))
+    post('menu/create', JSON.generate(menu))
+  end
+
+  def menu_addconditional menu
+    post('menu/addconditional', JSON.generate(menu))
+  end
+
+  def menu_delconditional menuid
+    post('menu/delconditional', JSON.generate({menuid: menuid}))
+  end
+
+  def menu_trymatch user_id
+    post('menu/trymatch', JSON.generate({user_id: user_id}))
   end
 
   def media media_id
-    response = get "media/get", params:{media_id: media_id}, base: FILE_BASE, as: :file
+    response = get 'media/get', params:{media_id: media_id}, base: FILE_BASE, as: :file
   end
 
   def media_create type, file
-    post "media/upload", {upload:{media: file}}, params:{type: type}, base: FILE_BASE
+    post 'media/upload', {upload:{media: file}}, params:{type: type}, base: FILE_BASE
   end
 
   def custom_message_send message
-    post "message/custom/send", JSON.generate(message), content_type: :json
+    post 'message/custom/send', JSON.generate(message), content_type: :json
   end
 
   def message_preview message
-    post "message/mass/preview", JON.generate(message), content_type: :json
+    post 'message/mass/preview', JON.generate(message), content_type: :json
   end
 
   def qrcode_create_scene(scene_id, expire_seconds = 604800)
@@ -69,29 +81,61 @@ class Wechat::Api
   end
 
   def template_message_send message
-    post "message/template/send", JSON.generate(message), content_type: :json
+    post 'message/template/send', JSON.generate(message), content_type: :json
   end
 
   def batchget_material(options = {})
-    post "material/batchget_material", JSON.generate(options), content_type: :json
+    post 'material/batchget_material', JSON.generate(options), content_type: :json
   end
 
   def get_material(media_id)
-    post "material/get_material", JSON.generate({ media_id: media_id }), content_type: :json
+    post 'material/get_material', JSON.generate({ media_id: media_id }), content_type: :json
   end
 
   def shorturl(long_url)
-    post "shorturl", JSON.generate({ action: 'long2short', long_url: long_url }), content_type: :json
+    post 'shorturl', JSON.generate({ action: 'long2short', long_url: long_url }), content_type: :json
   end
 
   def jsapi_ticket
     if ticket = Redis.current.get(:jsapi_ticket)
       ticket
     else
-      data = get("ticket/getticket", { params: { type: 'jsapi' } })
-      Redis.current.setex :jsapi_ticket, data["expires_in"] - 5, data["ticket"]
-      data["ticket"]
+      data = get('ticket/getticket', { params: { type: 'jsapi' } })
+      Redis.current.setex :jsapi_ticket, data['expires_in'] - 5, data['ticket']
+      data['ticket']
     end
+  end
+
+  def tag_create tag_name
+    post 'tags/create', JSON.generate({ tag: { name: tag_name }})
+  end
+
+  def tags
+    get 'tags/get'
+  end
+
+  def tag_update tagid, tag_name
+    post 'tags/update', JSON.generate({ tag: { id: tagid,  name: tag_name }})
+  end
+
+  def tag_delete tagid
+    post 'tags/delete', JSON.generate({ tag: { id: tagid }})
+  end
+
+  def tag_users tagid, next_openid = ''
+    post 'user/tag/get', JSON.generate({ tagid: tagid, next_openid: next_openid })
+  end
+
+  def batchtagging openid_list, tagid
+    post 'tags/members/batchtagging', JSON.generate({ openid_list: openid_list, tagid: tagid })
+  end
+
+  def batchuntagging openid_list, tagid
+    post 'tags/members/batchuntagging', JSON.generate({ openid_list: openid_list, tagid: tagid })
+  end
+
+  def user_tags openid
+    post 'tags/getidlist', JSON.generate({ openid: openid })
   end
 
   protected
